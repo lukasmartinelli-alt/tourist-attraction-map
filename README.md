8227774e0b6c5aa25ce3f5bed8b25d83ed5bf18d
Lukas Martinelli <me@lukasmartinelli.ch>
2018-04-13 21:13:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Landmarks for Maps
+
+A landmark is a recognizable natural or artificial feature used for navigation and orientation.
+This repository collects [Wikidata](https://www.wikidata.org/wiki/Q2319498) entities that
+are landmarks for cartographic use.
+
+
---
+++ bin/openlandmarks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict';
+const program = require('commander');
+const commands = require('../lib/commands');
+
+program
+  .description('')
+  .command('to-wikidata')
+  .action(function () {
+		commands.toWikidata(process.stdin, process.stdout);
+  });
+
+program
+  .command('to-geojson')
+  .action(function () {
+		commands.toGeoJSON(process.stdin, process.stdout);
+  });
+
+program
+  .command('to-collection')
+  .action(function () {
+		commands.toCollection(process.stdin, function(collection) {
+      console.log(JSON.stringify(collection));
+    });
+  });
+
+program.parse(process.argv);
